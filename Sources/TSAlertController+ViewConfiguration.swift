4f78dd1a5e0cf7f287d42e4abc83eb937fff1474
김소월 <rlarjsdn3@naver.com>
2025-01-23 21:33:32 +0900

    feat: `TSAlertController` 기본 구현 및 `TSAlertConfiguration`, `TSAlertViewConfiguration` 등 설정 코드 구현 (#2)
    * feat: TSAlertController기본구현 및 Configuration관련코드작성
    
    * feat: `title` 프로퍼티 오버라이딩 및 일부 프로퍼티 이름 수정

---
+++ Sources/TSAlertController+ViewConfiguration.swift
@@ -0,0 +1,156 @@
+// Copyright (c) 2025 rlarjsdn3 <rlarjsdn3@naver.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+
+public extension TSAlertController {
+    
+    ///
+    struct ViewConfiguration {
+        
+        // MARK: - Properties
+        
+        ///
+        public var titleTextAttributes: [NSAttributedString.Key: Any]?
+        
+        ///
+        public var titleTextAlignment: NSTextAlignment
+        
+        ///
+        public var titleNumberOfLines: Int
+        
+        ///
+        public var messageTextAttributes: [NSAttributedString.Key: Any]?
+        
+        ///
+        public var messageTextAligngn: NSTextAlignment
+        
+        ///
+        public var messageNumberOfLines: Int
+        
+        ///
+        public var backgroundColor: Background?
+        
+        ///
+        public var backgroundBorderColor: UIColor?
+        
+        ///
+        public var backgroundBorderWidth: CGFloat
+        
+        ///
+        public var alertCornerRadius: CGFloat
+        
+        ///
+        public var dimmedBackgroundViewColor: Background?
+        
+        ///
+        public var magin: LayoutMargin
+        
+        ///
+        public var size: CGSize
+        
+        
+        // MARK: - Initalizer
+        
+        public init(titleTextAttributes: [NSAttributedString.Key : Any]? = nil,
+                    titleTextAlignment: NSTextAlignment = .left,
+                    titleNumberOfLines: Int = 1,
+                    messageTextAttributes: [NSAttributedString.Key : Any]? = nil,
+                    messageTextAligngn: NSTextAlignment = .left,
+                    messageNumberOfLines: Int = 3,
+                    backgroundColor: Background? = .color(),
+                    backgroundBorderColor: UIColor? = nil,
+                    backgroundBorderWidth: CGFloat = 0,
+                    alertCornerRadius: CGFloat = 10,
+                    dimmedBackgroundViewColor: Background? = .color(alpha: 0.5),
+                    magin: LayoutMargin = .init(),
+                    size: CGSize = .init(width: 300, height: 300)) {
+            self.titleTextAttributes = titleTextAttributes
+            self.titleTextAlignment = titleTextAlignment
+            self.titleNumberOfLines = titleNumberOfLines
+            self.messageTextAttributes = messageTextAttributes
+            self.messageTextAligngn = messageTextAligngn
+            self.messageNumberOfLines = messageNumberOfLines
+            self.backgroundColor = backgroundColor
+            self.backgroundBorderColor = backgroundBorderColor
+            self.backgroundBorderWidth = backgroundBorderWidth
+            self.alertCornerRadius = alertCornerRadius
+            self.dimmedBackgroundViewColor = dimmedBackgroundViewColor
+            self.magin = magin
+            self.size = size
+        }
+        
+        
+    }
+}
+
+
+public extension TSAlertController.ViewConfiguration {
+    
+    ///
+    enum Background {
+        
+        ///
+        case effect(UIVisualEffect)
+        
+        ///
+        case color(UIColor = .systemBackground,
+                   alpha: CGFloat = 1.0)
+    }
+}
+
+
+public extension TSAlertController.ViewConfiguration {
+    
+    ///
+    struct LayoutMargin {
+        
+        // MARK: - Properties
+        
+        ///
+        public var contentTop: CGFloat
+        
+        ///
+        public var contentBottom: CGFloat
+        
+        ///
+        public var contentLeft: CGFloat
+        
+        ///
+        public var contentRight: CGFloat
+        
+        
+        // MARK: - Intializer
+        
+        ///
+        public init(contentTop: CGFloat = 15,
+                    contentBottom: CGFloat = 15,
+                    contentLeft: CGFloat = 15,
+                    contentRight: CGFloat = 15) {
+            self.contentTop = contentTop
+            self.contentBottom = contentBottom
+            self.contentLeft = contentLeft
+            self.contentRight = contentRight
+        }
+        
+    }
+    
+}
